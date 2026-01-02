f94815a671cd73503dce42e17c66406a79715198
Suyash Baoney <suyashbaoney58@gmail.com>
2026-01-03 00:00:38 +0530

    added Home button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 // 
 
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../Images/jsmentorlogo.png';
 import "./Navbar.css";
 
 const NavbarComponent = () => {
+    const location = useLocation();
+    const isHome = location.pathname === '/';
+
     return (
         <Navbar bg="light" expand="lg" sticky="top" className="navbar-custom">
             <Container fluid>
@@ -29,6 +33,11 @@ const NavbarComponent = () => {
                 <Navbar.Collapse id="navbarSupportedContent">
                     {/* Navigation Links */}
                     <Nav className="ms-auto mb-2 mb-lg-0 nav-links-container">
+                        {!isHome && (
+                            <Nav.Link href="/" className="nav-link">
+                                Home
+                            </Nav.Link>
+                        )}
                         <Nav.Link href="/#learning-paths" className="nav-link">
                             Learning Paths
                         </Nav.Link>
