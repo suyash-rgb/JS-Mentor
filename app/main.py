7aba222cd61f109c0ede0d74f68419ac6c060d87
Suyash Baoney <suyashbaoney58@gmail.com>
2026-01-27 15:12:41 +0530

    fix CORS issue

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from app import models, routers
 from app.database import engine, Base
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="JS Mentor Backend")
+
+# defining allowed origins for CORS
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1.3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"], # Allows all headers (including Authorization for JWT)
+)
 
 # Create all tables in the database
 models.Base.metadata.create_all(bind=engine)
