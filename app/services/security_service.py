e6727db3f4a3abf5639102fbf9d61ea83ae80fc3
Suyash Baoney <suyashbaoney58@gmail.com>
2026-01-27 12:46:32 +0530

    rem deprecated timestamp handling

--- app/services/security_service.py
+++ app/services/security_service.py
@@ -1,16 +1,16 @@
 from jose import jwt
 import os
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 # Standard JWT setup
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("JWT_ALGORITHM")
 
-def create_jwt_token(data: dict):
+def create_jwt_token(data: dict):   
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=60)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=60)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
